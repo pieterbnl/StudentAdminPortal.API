c517324c9aa98a512a95f0a55309355f04e16364
Pieter B <pieterbeernink@hotmail.com>
2022-07-27 12:21:13 +0200

    make some model fields nullable, run migrations and create student repo

--- StudentAdminPortal.API/DataModels/Address.cs
+++ StudentAdminPortal.API/DataModels/Address.cs
@@ -3,11 +3,11 @@
     public class Address
     {
         public Guid Id { get; set; }
-        public string PhysicalAddress { get; set; }
-        public string PostalAddress { get; set; }
+        public string? PhysicalAddress { get; set; }
+        public string? PostalAddress { get; set; }
 
         // Navigation property
-        public Student StudentId { get; set; }
+        public Guid StudentId { get; set; }
 
     }
 }
---
+++ StudentAdminPortal.API/Repositories/SqlStudentRespository.cs
@@ -0,0 +1,18 @@
+﻿using StudentAdminPortal.API.DataModels;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRespository : IStudentRepository
+    {
+        private readonly StudentAdminContext _context;
+
+        public SqlStudentRespository(StudentAdminContext context)
+        {
+            _context = context;
+        }
+        public List<Student> GetStudents()
+        {
+            return _context.Student.ToList();
+        }
+    }
+}
