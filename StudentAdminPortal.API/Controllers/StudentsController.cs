f8288bb7e693bb6c838eb65f3da7a4eca46800f3
Pieter B <pieterbeernink@hotmail.com>
2022-07-27 13:02:14 +0200

    create student API controller

---
+++ StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.Repositories;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentsController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        [HttpGet]
+        [Route("[controller")] // gives controller name: students
+        public IActionResult GetAllStudents()
+        {
+            return Ok(_studentRepository.GetStudents());
+        }
+    }
+}
--- StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ var connectionString = builder.Configuration.GetConnectionString("StudentAdminPo
 
 builder.Services.AddDbContext<StudentAdminContext>(options => 
     options.UseSqlServer(connectionString));
+
+// Inject dependencies inside the service - giving an implementation of SqlStudentRespository when called
+builder.Services.AddScoped<IStudentRepository, SqlStudentRespository>(); 
 
 var app = builder.Build();
 
